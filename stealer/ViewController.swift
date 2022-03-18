9e4df15ebb9ab05bbd9034be9c4af190b7075370
Misha Causur <mishacausur@mail.ru>
2022-03-18 14:23:33 +0500

    Initial Commit

---
+++ stealer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stealer
+//
+//  Created by Misha Causur on 18.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
